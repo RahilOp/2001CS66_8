c67389096b9632a6c6721e4902cf9464a704619f
SYED ALI ABBAS RAHIL <76390003+RahilOp@users.noreply.github.com>
2024-05-11 21:55:49 +0530

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,13 @@
+code_path = "../code"
+dataset_path = "../dataset"
+output_path = "../output"
+
+dataset_name = "NSL_KDD_2009"
+train_dataset_name = 'NSL_KDD_2009_preprocessed_train_data.csv'
+test_dataset_name = 'NSL_KDD_2009_preprocessed_test_data.csv'
+
+# percent of train set for optimization
+opt_percent = 0.2   # 20% 
+
+# number of iteration for opt
+no_of_opt_iteration = 15 # fix, so don't change its only for testing
